72ff395b9873ae11a879afcc845873d61fd394da
Erik Schwartz <erik@erikschwartz.net>
2015-04-12 11:00:14 -0400

    Add tweetbeam

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var widgetHtml = function(widget, columnSize) {
   return '<div class="widget col col-md-' + columnSize + '">'
    + '  <div class="panel panel-default">'
    + '    <div class="panel-heading">'
-   + '      <h3 class="panel-title">' + widgetTitle + '</h3>'
+   + '      <h3 class="panel-title"><a href="' + widgetUrl + '" target="_blank">' + widgetTitle + '</a></h3>'
    + '    </div>'
    + '    <div class="panel-body">'
    + '      <div class="embed-responsive embed-responsive-16by9">'
@@ -28,6 +28,10 @@ var widgetsByName = {
     title: "Weekly building permits",
     url: "http://www.charted.co/?{%22dataUrl%22%3A%22https%3A%2F%2Fdl.dropboxusercontent.com%2Fu%2F70416620%2Fbuilding-permits-year-over-year.csv%22%2C%22color%22%3A%22dark%22%2C%22charts%22%3A[{%22title%22%3A%22Weekly%20Permits%22}]}"
   },
+  'tweetbeam': {
+    title: 'Twitter activity',
+    url: 'http://www.tweetbeam.com/show?query=lexington%2C%20ky'
+  }
 };
 var rowHtml = function(widgets) {
   var colWidth = 6;
