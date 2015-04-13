f46c3ac750269056143741d7c7deb2c85fad422a
Erik Schwartz <erik@erikschwartz.net>
2015-04-13 17:44:51 -0400

    Add 2 twitters @ 3 col each

--- main.js
+++ main.js
@@ -1,28 +1,18 @@
 var widgetHtml = function(widget, columnSize) {
-  // var widgetTitle = widget.title;
-  // var widgetUrl = widget.url;
-  // var titleHtml = '';
-  //
-  // if (widgetTitle) {
-  //   titleHtml = '    <div class="panel-heading">'
-  //    + '      <h3 class="panel-title"><a href="' + widgetUrl + '" target="_blank">' + widgetTitle + '</a></h3>'
-  //    + '    </div>'
-  //  }
-  //
-  // return '<div class="widget col col-md-' + columnSize + '">'
-  //  + '  <div class="panel panel-default">'
-  //  + '    <div class="panel-body">'
-  //  + titleHtml
-  //  + '      <div class="embed-responsive embed-responsive-16by9">'
-  //  + '        <iframe seamless="seamless" scrolling="no" class="embed-responsive-item" src="' + widgetUrl + '" style="font-size: 50%"></iframe>'
-  //  + '      </div>'
-  //  + '    </div>'
-  //  + '  </div>'
-  //  + '</div>'
   var widgetTitle = widget.title;
   var widgetUrl = widget.url;
   var titleHtml = '';
 
+  if (widgetUrl === 'twitter-1') {
+    return '<div class="widget col col-md-3 text-center">'
+    + '<a height="1500" data-chrome="nofooter noscrollbar" class="twitter-timeline" href="https://twitter.com/search?q=lexington%2C%20ky" data-widget-id="587634091325050880">Tweets about lexington, ky</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>'
+    + '</div>';
+  }
+  if (widgetUrl === 'twitter-2') {
+    return '<div class="widget col col-md-3 text-center">'
+      + '<a data-chrome="nofooter noscrollbar" class="twitter-timeline" href="https://twitter.com/search?q=%40jimgraylexky" data-widget-id="587731792108646400">Tweets about @jimgraylexky</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>'
+    + '</div>';
+  }
   if (widgetTitle) {
     titleHtml = '    <div class="panel-heading">'
      + '      <h3 class="panel-title"><a href="' + widgetUrl + '" target="_blank">' + widgetTitle + '</a></h3>'
@@ -58,8 +48,11 @@ var widgetsByName = {
     title: 'Twitter Activity',
     url: 'http://www.tweetbeam.com/show?query=lexington%2C%20ky'
   },
-  'twitter-timeline': {
-    url: 'twitter.html',
+  'twitter-1': {
+    url: 'twitter-1',
+  },
+  'twitter-2': {
+    url: 'twitter-2',
   },
   'traffic': {
     title: 'Traffic Overview',
@@ -73,7 +66,7 @@ var widgetsByName = {
 var rowHtml = function(widgets) {
   var colWidth = 6;
   var widgets = _.map(widgets, function(widget) {
-    return widgetHtml(widget, colWidth);
+    return widgetHtml(widget, widget.colWidth || colWidth);
   });
   return '<div class="row top-buffer">' + widgets.join('') + '</div>';
 };
